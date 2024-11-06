0cad88c8f7090d3095c1a348bb84627db98371cd
Amaurys De Los Santos Mendez <amaurysdlsm@gmail.com>
2024-11-06 16:04:08 -0500

    Created a basic Upload doc view

--- app/src/main/java/com/example/flashquizzer/model/Flashcard.kt
+++ app/src/main/java/com/example/flashquizzer/model/Flashcard.kt
@@ -1,3 +1,3 @@
 package com.example.flashquizzer.model
 
-data class Flashcard ( val front: String, val back: String,var currentSide: String = front )
+data class Flashcard(val front: String, val back: String, var currentSide: String = front)
--- app/src/main/java/com/example/flashquizzer/ui/uploaddoc/UploadDocViewmodel.kt
+++ app/src/main/java/com/example/flashquizzer/ui/uploaddoc/UploadDocViewmodel.kt
@@ -3,4 +3,7 @@ package com.example.flashquizzer.ui.uploaddoc
 import androidx.lifecycle.ViewModel
 
 class UploadDocViewmodel : ViewModel() {
+    fun UploadDoc() {
+        // TODO: Implement the business logic for uploading a document
+    }
 }
