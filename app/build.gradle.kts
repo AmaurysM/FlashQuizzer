84c7c7db32e4e9d6c5ecc0e46e86d8b7044a5096
Brett Silver <bsilv22@yahoo.com>
2024-11-06 20:22:20 -0500

    Added the upload text document button function

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,11 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.google.firebase:firebase-storage-ktx:20.2.0")
+    implementation ("androidx.navigation:navigation-compose:2.7.5")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation ("androidx.activity:activity-compose:1.7.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -54,6 +58,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.firebase.auth)
     implementation(libs.androidx.runtime.livedata)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/flashquizzer/navigation/FlashQuizzerDestinations.kt
+++ app/src/main/java/com/example/flashquizzer/navigation/FlashQuizzerDestinations.kt
@@ -1,11 +1,11 @@
 package com.example.flashquizzer.navigation
 
-sealed class FlashQuizzerDestinations(val route: String, val title: String = "") {
-    data object Home : FlashQuizzerDestinations("home")
-    data object UploadDoc : FlashQuizzerDestinations("upload_doc")
-    data object ViewFlashcards : FlashQuizzerDestinations("view_flashcards", "Flashcards")
-    data object TakeQuiz : FlashQuizzerDestinations("take_quiz", "Quiz")
-    data object Login : FlashQuizzerDestinations("login")
-    data object Register : FlashQuizzerDestinations("register")
 
-}
+
+sealed class FlashQuizzerDestinations(val route: String) {
+    object Home : FlashQuizzerDestinations("home")
+    object UploadDoc : FlashQuizzerDestinations("upload_doc")
+    object ViewFlashcards : FlashQuizzerDestinations("view_flashcards")
+    object TakeQuiz : FlashQuizzerDestinations("take_quiz")
+    object Login : FlashQuizzerDestinations("login")
+    object Register : FlashQuizzerDestinations("register")}
