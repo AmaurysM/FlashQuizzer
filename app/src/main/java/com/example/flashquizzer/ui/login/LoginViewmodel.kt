36650a9fd96f40758b571e7aad439fa6b0040b2e
Amaurys De Los Santos Mendez <amaurysdlsm@gmail.com>
2024-11-20 12:25:02 -0500

    Updated the Login View.

--- app/src/main/java/com/example/flashquizzer/ui/login/LoginViewmodel.kt
+++ app/src/main/java/com/example/flashquizzer/ui/login/LoginViewmodel.kt
@@ -5,12 +5,27 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavHostController
+import com.example.flashquizzer.R
 import com.example.flashquizzer.model.AuthManager
 import com.example.flashquizzer.model.LoginData
 import com.example.flashquizzer.navigation.FlashQuizzerDestinations
 
 class LoginViewmodel : ViewModel() {
+    var passwordVisible by mutableStateOf(false)
     var loginData by mutableStateOf(LoginData("", ""))
+
+    fun iconPassword(): Int {
+        return if (passwordVisible) {
+            R.drawable.baseline_visibility_24
+        } else {
+            R.drawable.baseline_visibility_off_24
+        }
+    }
+
+
+    fun togglePasswordVisibility() {
+        passwordVisible = !passwordVisible
+    }
 
     fun login() {
         AuthManager.login(loginData, loginInputEmpty())
@@ -23,5 +38,9 @@ class LoginViewmodel : ViewModel() {
 
     fun goHome(navHostController: NavHostController) {
         navHostController.navigate(FlashQuizzerDestinations.Home.route)
+    }
+
+    fun goToRegister(navHostController: NavHostController) {
+        navHostController.navigate(FlashQuizzerDestinations.Register.route)
     }
 }
